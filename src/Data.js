dc97a9f856c7e430931629f485915eeec5763172
KeplerInCoding <shreyakv8@gmail.com>
2023-11-29 00:22:42 +0530

    data

---
+++ src/Data.js
@@ -0,0 +1,114 @@
+const Data = [
+    // Fears related to animals
+    {
+      group: "Fears related to animals",
+      phobias: [
+        {
+          id: 1,
+          name: "Arachnophobia",
+          meaning: "Fear of spiders",
+          description: "Arachnophobia is the intense fear of spiders and other arachnids. It can lead to panic attacks and avoidance of places where spiders may be present.",
+          image: "https://example.com/arachnophobia.jpg",
+        },
+        {
+          id: 2,
+          name: "Cynophobia",
+          meaning: "Fear of dogs",
+          description: "Cynophobia is the irrational fear of dogs. It may develop due to a traumatic experience with a dog or for reasons unknown.",
+          image: "https://example.com/cynophobia.jpg",
+        },
+        // Add more phobias related to animals
+      ],
+    },
+  
+    // Fears related to the natural environment
+    {
+      group: "Fears related to the natural environment",
+      phobias: [
+        {
+          id: 3,
+          name: "Acrophobia",
+          meaning: "Fear of heights",
+          description: "Acrophobia is the fear of heights, which can range from mild discomfort to severe anxiety and panic attacks when exposed to heights.",
+          image: "https://example.com/acrophobia.jpg",
+        },
+        {
+          id: 4,
+          name: "Astraphobia",
+          meaning: "Fear of thunder and lightning",
+          description: "Astraphobia is the fear of thunder and lightning. It can lead to anxiety and avoidance of storms.",
+          image: "https://example.com/astraphobia.jpg",
+        },
+        // Add more phobias related to the natural environment
+      ],
+    },
+  
+    // Fears related to blood, injury, or medical issues
+    {
+      group: "Fears related to blood, injury, or medical issues",
+      phobias: [
+        {
+          id: 5,
+          name: "Trypanophobia",
+          meaning: "Fear of injections",
+          description: "Trypanophobia is the fear of injections or needles. It may be associated with a fear of medical procedures or a traumatic experience.",
+          image: "https://example.com/trypanophobia.jpg",
+        },
+        {
+          id: 6,
+          name: "Traumatophobia",
+          meaning: "Fear of injury",
+          description: "Traumatophobia is the fear of injury or physical harm. It can be triggered by the thought of accidents or harm to oneself or others.",
+          image: "https://example.com/traumatophobia.jpg",
+        },
+        // Add more phobias related to blood, injury, or medical issues
+      ],
+    },
+  
+    // Fears related to specific situations
+    {
+      group: "Fears related to specific situations",
+      phobias: [
+        {
+          id: 7,
+          name: "Aviophobia",
+          meaning: "Fear of flying",
+          description: "Aviophobia is the fear of flying. It can range from mild anxiety to severe panic attacks when faced with the prospect of air travel.",
+          image: "https://example.com/aviophobia.jpg",
+        },
+        {
+          id: 8,
+          name: "Claustrophobia",
+          meaning: "Fear of enclosed spaces",
+          description: "Claustrophobia is the fear of enclosed spaces. It can be triggered by situations such as being in an elevator or small room.",
+          image: "https://example.com/claustrophobia.jpg",
+        },
+        // Add more phobias related to specific situations
+      ],
+    },
+  
+    // Other phobias
+    {
+      group: "Other",
+      phobias: [
+        {
+          id: 9,
+          name: "Cherophobia",
+          meaning: "Fear of happiness",
+          description: "Cherophobia is the fear of happiness. Individuals with this phobia may avoid situations that bring joy or happiness.",
+          image: "https://example.com/cherophobia.jpg",
+        },
+        {
+          id: 10,
+          name: "Ligyrophobia",
+          meaning: "Fear of loud noises",
+          description: "Ligyrophobia is the fear of loud noises. It can lead to anxiety and avoidance of situations with loud sounds.",
+          image: "https://example.com/ligyrophobia.jpg",
+        },
+        // Add more phobias in the 'Other' category
+      ],
+    },
+  ];
+  
+  export default Data;
+  
