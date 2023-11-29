7b29f6894f721ef745c46938a8b64842bd201d8d
KeplerInCoding <shreyakv8@gmail.com>
2023-11-29 16:45:01 +0530

    cards done

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -10,18 +10,17 @@ const CategoryPage = () => {
   
 
   return (
-    <div>
-        <div>
-            
-        </div>
+    <div className='w-screen flex flex-col items-center bg-black'>
         {data.map((phobias, index) =>
         phobias && phobias.hasOwnProperty(`${category}`)
           ? phobias[`${category}`].map((phobia) => 
-        <div>
-          <h1>{phobia.name}</h1>
-          <h2>{phobia.meaning}</h2>
-          <h3>{phobia.description}</h3>
-          <img src={phobia.image} alt={phobia.name}></img>
+        <div class="m-4 flex items-center border-2 border-white rounded-md w-[800px] p-10 bg-gradient-to-br from-slate-900 to-red-900 shadow-lg shadow-red-900">
+          <div>
+          <h1 className=' text-3xl font-extrabold text-red-200 mb-5'>{phobia.name}</h1>
+          <h2 className='text-red-500 font-mono mb-2'>"{phobia.meaning}"</h2>
+          <h3 className=' text-red-300 text-lg'>{phobia.description}</h3>
+          </div>
+          <img className='m-4 border-white h-56 rounded-sm border-2' src={phobia.image} alt={phobia.name}></img>
         </div>)
           : null
       )}
