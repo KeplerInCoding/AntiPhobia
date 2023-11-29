45e1430831592ac09d52787cd9f1ecdd234037b5
KeplerInCoding <shreyakv8@gmail.com>
2023-11-29 11:14:41 +0530

    phobias page

--- src/pages/Phobias.js
+++ src/pages/Phobias.js
@@ -25,15 +25,21 @@ const Phobias = () => {
           </div>
         </div>
 
-        <div className='relative top-[-450px] left-[30%] text-4xl'>Fears related to animals</div>
-        <div className='relative top-[-475px] left-[58%]'>Fears related to the natural environment</div>
-        <div className='relative top-[-315px] left-[10%]'>Fears related to blood, injury, or medical issues</div>
-        <div className='relative top-[-340px] left-[65%]'>Fears related to specific situations</div>
-        <div className='relative top-[-200px] left-[33%]'>Other Phobias</div>
-        <div className='relative top-[-220px] left-[60%]'>All Phobias</div>
-
-
 </body>
+
+        <div className='w-screen h-screen absolute flex place-content-between top-32 '>
+          <div className='flex gap-28 flex-col ml-10'>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>Fears related to animals</div>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>Fears related to blood, injury, or medical issues</div>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>Other Phobias</div>
+          </div>
+
+          <div className='flex gap-28 flex-col mr-10'>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>Fears related to the natural environment</div>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>Fears related to specific situations</div>
+            <div className=' bg-gradient-to-tr from-slate-800 text-center font-mono text-xl rounded-full shadow-lg shadow-orange-900 hover:scale-125 hover:shadow-xl hover:shadow-orange-800 font-extrabold to-red-900 p-3 w-96 duration-200'>All Phobias</div>
+          </div>
+        </div>
       
     </div>
   )
