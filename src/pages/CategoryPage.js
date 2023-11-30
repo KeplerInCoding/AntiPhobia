0df6d1314394cf676ed1c446b2f834dfeff89717
KeplerInCoding <shreyakv8@gmail.com>
2023-11-30 22:31:37 +0530

    css updated

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -16,9 +16,9 @@ const CategoryPage = () => {
           ? phobias[`${category}`].map((phobia) => 
         <div class="m-4 flex items-center border-2 border-white rounded-md w-[800px] p-5 py-3 bg-gradient-to-br from-slate-100 to-gray-300 shadow-lg shadow-blue-900">
           <div>
-          <h1 className=' text-3xl font-extrabold text-black mb-5'>{phobia.name}</h1>
+          <h1 className=' text-3xl font-extrabold text-gray-700 mb-5'>{phobia.name}</h1>
           <h2 className='text-black/70 font-mono mb-2'>"{phobia.meaning}"</h2>
-          <h3 className=' text-black text-lg'>{phobia.description}</h3>
+          <h3 className=' text-gray-600 text-lg'>{phobia.description}</h3>
           </div>
           <img className='m-4 border-white h-48 rounded-sm border' src={phobia.image} alt={phobia.name}></img>
         </div>)
