daedcd68b7bc89d47c22d86c4d3a3e615960c08f
KeplerInCoding <shreyakv8@gmail.com>
2023-11-30 21:45:41 +0530

    explore css

--- src/pages/Explore_VR.js
+++ src/pages/Explore_VR.js
@@ -2,67 +2,81 @@ import React from "react";
 
 const ExploreVR = () => {
   return (
-    <div>
-      <h1>Fear of Heights</h1>
+    <div className="h-fit mt-24 bg-gradient-to-r from-slate-100 to-blue-200">
+      <div className="flex flex-col w-screen items-center p-10 gap-5 ">
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Acrophobia : Fear of Heights</h1>
       <iframe
-        width="300"
-        height="200"
+        width="600"
+        height="400"
         src="https://www.youtube.com/embed/rNVpFXuAXIA?si=DBZkJWqHWAAdEmcT"
         title="YouTube video player"
         frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allow="accelerometer; autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen="allowfullscreen"
       ></iframe>
-      <h1>Dog wal fear</h1>
+      </div>
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Cynophobia : Fear of dogs</h1>
       <iframe
-      width="300"
-      height="200"
+      width="600"
+      height="400"
         src="https://www.youtube.com/embed/nLjabvojUhk?si=JJALYyDb2ezY3VCd"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen"
         allowfullscreen="allowfullscreen"
       ></iframe>
-      <h1>ALone wala Fear Cautasutrophobia</h1>
+      </div>
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Claustrophobia : Fear of enclosed spaces</h1>
       <iframe
-       width="300"
-       height="200"
+       width="600"
+       height="400"
         src="https://www.youtube.com/embed/bVJMNJiIUGU?si=1wrnDYUPKLvzQ1Lr"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share ;fullscreen"
         allowfullscreen="allowfullscreen"
       ></iframe>
-      <h1>Lightning Fear</h1>
+      </div>
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Astraphobia : Fear of lightning</h1>
       <iframe
-        width="300"
-        height="200"
+        width="600"
+        height="400"
         src="https://www.youtube.com/embed/NW90-qo_nhc?si=YvKbDCExxxUcU3Zw"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen="allowfullscreen"
       ></iframe>
-      <h1>Spider Fear</h1>
+      </div>
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Arachnophobia : Fear of spiders</h1>
       <iframe
-       width="300"
-       height="200"
+       width="600"
+       height="400"
         src="https://www.youtube.com/embed/5H-4mfmiKP8?si=qcbiF097Mbw7ygit"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen="allowfullscreen"
       ></iframe>
-      <h1>Public Speaking</h1>
+      </div>
+      <div className="flex place-content-between items-center w-full bg-gradient-to-t from-slate-200 to bg-slate-300 border border-black p-5 px-10 rounded-sm shadow-sm shadow-gray-600">
+      <h1 className=" font-extrabold text-4xl text-gray-700">Public Speaking</h1>
       <iframe
-        width="300"
-        height="200"
+        width="600"
+        height="400"
         src="https://www.youtube.com/embed/wU0aFi5avsc?si=Hs0xra8dYjWj1x0L"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen="allowfullscreen"
       ></iframe>
+      </div>
+      </div>
     </div>
   );
 };
