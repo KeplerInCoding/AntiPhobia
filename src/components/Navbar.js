4e62de81dab5c0cebe2b3c8947396fbc1fb2686e
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2023-11-30 09:49:53 +0530

    Explore Page Started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../assets/AntiPhobia.png";
 
 const Navbar = () => {
   return (
-    <div className='absolute top-0 z-40 w-screen'>
+    <div className='relative top-0 z-40 w-screen'>
 
 <div className='flex h-24 place-content-evenly gap-52 items-center py-2  bg-gradient-to-t from-slate-100/5 to-blue-400 backdrop-blur-sm'>
 <div>
@@ -12,7 +12,7 @@ const Navbar = () => {
       
     </div>
     <nav>
-      <ul className='flex flex-wrap place-content-evenly gap-10 text-blue-900 text-3xl font-mono font-extrabold pl-10'>
+      <ul className='flex flex-wrap place-content-evenly gap-10 text-blue-900 text-3xl font-mono font-extrabold pl-10 '>
         <li>
           <NavLink className="hover:text-blue-800" exact activeClassName="active" to='/'>Home</NavLink>
         </li>
--- src/pages/Explore_VR.js
+++ src/pages/Explore_VR.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
 const ExploreVR = () => {
   return (
-    <div>Explore_VR</div>
+    <div>
+      <h1>Fear of Heights</h1>
+       <iframe width="250" height="200" src="https://www.youtube.com/embed/rNVpFXuAXIA?si=DBZkJWqHWAAdEmcT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       <h1>Dog wal fear</h1>
+       <iframe width="250" height="200" src="https://www.youtube.com/embed/nLjabvojUhk?si=JJALYyDb2ezY3VCd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
   )
 }
 
