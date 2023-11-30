fed9d8583821f23df8ca6a0e2a66436f0777a740
Mohd Musaiyab <mohdmusaiyab123@gmail.com>
2023-11-30 11:06:29 +0530

    Videos Uploaded

--- src/pages/Explore_VR.js
+++ src/pages/Explore_VR.js
@@ -1,14 +1,70 @@
-import React from 'react'
+import React from "react";
 
 const ExploreVR = () => {
   return (
     <div>
       <h1>Fear of Heights</h1>
-       <iframe width="250" height="200" src="https://www.youtube.com/embed/rNVpFXuAXIA?si=DBZkJWqHWAAdEmcT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-       <h1>Dog wal fear</h1>
-       <iframe width="250" height="200" src="https://www.youtube.com/embed/nLjabvojUhk?si=JJALYyDb2ezY3VCd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <iframe
+        width="300"
+        height="200"
+        src="https://www.youtube.com/embed/rNVpFXuAXIA?si=DBZkJWqHWAAdEmcT"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
+      <h1>Dog wal fear</h1>
+      <iframe
+      width="300"
+      height="200"
+        src="https://www.youtube.com/embed/nLjabvojUhk?si=JJALYyDb2ezY3VCd"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
+      <h1>ALone</h1>
+      <iframe
+       width="300"
+       height="200"
+        src="https://www.youtube.com/embed/bVJMNJiIUGU?si=1wrnDYUPKLvzQ1Lr"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share ;fullscreen"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
+      <h1>Lightning Fear</h1>
+      <iframe
+        width="300"
+        height="200"
+        src="https://www.youtube.com/embed/NW90-qo_nhc?si=YvKbDCExxxUcU3Zw"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
+      <h1>Spider Fear</h1>
+      <iframe
+       width="300"
+       height="200"
+        src="https://www.youtube.com/embed/5H-4mfmiKP8?si=qcbiF097Mbw7ygit"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
+      <h1>Public Speaking</h1>
+      <iframe
+        width="300"
+        height="200"
+        src="https://www.youtube.com/embed/wU0aFi5avsc?si=Hs0xra8dYjWj1x0L"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen="allowfullscreen"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default ExploreVR
+export default ExploreVR;
